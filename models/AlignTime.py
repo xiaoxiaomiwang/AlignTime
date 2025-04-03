051a31a8c1e19324d97a06203f5da6092007a972
xiaoxiaomiwang <152844411+xiaoxiaomiwang@users.noreply.github.com>
2025-04-03 10:02:10 +0800

    Update AlignTime.py

--- models/AlignTime.py
+++ models/AlignTime.py
@@ -26,7 +26,8 @@ class PASBlock(nn.Module):
         y = torch.zeros(shape, device=x_list[0].device)
         for idx, x_pad in enumerate(x_list):
             y[:, :, idx::self.highest_freq_period] = x_pad
-        return y
+
+        return y[:, :, :]
 
     def forward(self, x):
         x_samp = []
@@ -35,6 +36,7 @@ class PASBlock(nn.Module):
 
         x = self.FSABlock(x.shape, x_samp)
         return x
+
 
 class SamplingBlock(nn.Module):
     def __init__(self, tokens_dim, hidden_dim, highest_freq_period):
@@ -48,6 +50,7 @@ class SamplingBlock(nn.Module):
         y = y.transpose(1, 2)
         return y
 
+
 class PredictedProjection(nn.Module):
     def __init__(self, seq_len, pred_len):
         super().__init__()
@@ -57,24 +60,48 @@ class PredictedProjection(nn.Module):
         x = self.linears(x.transpose(1, 2)).transpose(1, 2)
         return x
 
+
 class AlignTime(nn.Module):
     def __init__(self, configs):
         super().__init__()
 
         self.highest_freq_period = configs.highest_freq_period
+        print(self.highest_freq_period)
+
+        self.seq_len = configs.seq_len
+        if self.seq_len % self.highest_freq_period == 0:
+            self.pad_len = self.seq_len
+        else:
+            self.pad_len = (self.seq_len // self.highest_freq_period + 1) * self.highest_freq_period
+
         self.sampling_block = nn.ModuleList([
-            SamplingBlock(configs.seq_len, configs.d_model, configs.highest_freq_period) for _ in range(configs.e_layers)
+            SamplingBlock(self.pad_len, configs.d_model, configs.highest_freq_period) for _ in range(configs.e_layers)
         ])
         self.norm = nn.LayerNorm(configs.enc_in)
         self.projection = PredictedProjection(configs.seq_len, configs.pred_len)
         self.rev = RevIN(configs.enc_in)
 
+    def pad_time_series(self, x):
+        batch_size, seq_len, num_features = x.shape
+        remainder = seq_len % self.highest_freq_period
+        if remainder == 0:
+            return x, seq_len
+
+        pad_len = (seq_len//self.highest_freq_period+1)*self.highest_freq_period - seq_len
+        start_period = self.highest_freq_period * (seq_len // self.highest_freq_period-1) - 1
+        end_period = start_period + self.highest_freq_period
+        last_full_period_data = x[:, start_period:end_period, :]
+        pad_data = last_full_period_data[:, -pad_len:, :]
+        x_padded = torch.cat([x, pad_data], dim=1)
+        return x_padded, seq_len
+
     def forward(self, x):
         x = self.rev(x, 'norm')
+        x_padded, pad_length = self.pad_time_series(x)
 
         for block in self.sampling_block:
-            x = block(x)
+            x = block(x_padded)
 
-        x = self.projection(x)
+        x = self.projection(x[:, :self.seq_len, :])
         x = self.rev(x, 'denorm')
         return x
